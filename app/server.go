d8d5900d2093d87e1f04822617103469f7b4c54b
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-03-14 15:55:17 +0100

    stage 6

--- app/server.go
+++ app/server.go
@@ -15,6 +15,92 @@ const (
 	NOT_FOUND_RESPONSE    = "HTTP/1.1 404 Not Found\r\n\r\n"
 )
 
+func HandleConnection(conn net.Conn) {
+	defer conn.Close()
+	req := ReadRequest(conn)
+
+	path, msg := ParsePath(req)
+
+	if path == "/" {
+		//200 OK status
+		conn.Write([]byte(OK_RESPONSE))
+	} else if strings.Contains(path, "echo") {
+
+		response := []byte(OK_RESPONSE_WITH_BODY + fmt.Sprintf("Content-Type: text/plain\r\nContent-length: %d\r\n\r\n%s\r\n", len(msg), msg))
+
+		conn.Write(response)
+
+	} else if strings.Contains(path, "user-agent") {
+		userAgent := ParseUserAgent(req)
+		response := []byte(OK_RESPONSE_WITH_BODY + fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n", len(userAgent), userAgent))
+
+		conn.Write(response)
+	} else {
+		//404 not found
+		conn.Write([]byte(NOT_FOUND_RESPONSE))
+	}
+}
+
+func ReadRequest(conn net.Conn) string {
+	data := make([]byte, 1024)
+
+	_, err := conn.Read(data)
+	if err != nil {
+		fmt.Println("Error getting data: ", err.Error())
+	}
+
+	return string(data)
+}
+
+/**
+* Parse the path
+ */
+func ParsePath(pathReq string) (string, string) {
+	parts := strings.Split(pathReq, " ")
+
+	if len(parts) < 2 {
+		fmt.Println("No path to extract")
+		os.Exit(1)
+	}
+
+	path := parts[1]
+
+	return GetMessageFromPath(path)
+}
+
+/*
+*Get the message from a given path
+ */
+func GetMessageFromPath(path string) (string, string) {
+	parts := strings.Split(path, "/")
+	if len(parts) < 3 {
+		return ("/" + parts[1]), ""
+	}
+	return ("/" + parts[1]), strings.Join(parts[2:], "/")
+}
+
+/*
+* Return the user agent from an http request
+ */
+func ParseUserAgent(s string) string {
+	parts := strings.Split(s, "\n")
+
+	if len(parts) < 3 {
+		fmt.Println("Inorrrect HTTP Request")
+		os.Exit(1)
+	}
+
+	for _, part := range parts {
+		if !strings.Contains(part, "User-Agent") {
+			continue
+		}
+
+		userAgent := strings.Split(part, ":")
+		return strings.TrimSpace(userAgent[1])
+	}
+	return ""
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -28,43 +114,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	connection, err := listen.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go HandleConnection(conn)
 	}
-
-	defer listen.Close()
-
-	data := make([]byte, 1024)
-
-	length, err := connection.Read(data)
-	if err != nil {
-		fmt.Println("Error getting data: ", err.Error())
-	}
-
-	requestContent := strings.Split(string(data[:length]), EOF_MAKER)
-	reqFirstLine := strings.Split(requestContent[0], " ") // request info
-	//reqSecondLine := strings.Split(requestContent[1], " ") // host info
-	reqThirdLine := strings.Split(requestContent[2], " ") // user-agent info
-
-	//method := reqFirstLine[0]
-	path := reqFirstLine[1]
-
-	//hostAddress := reqSecondLine[1]
-	userAgent := reqThirdLine[1]
-
-	var response []byte
-	if path == "/" {
-		response = []byte(OK_RESPONSE)
-	} else if strings.HasPrefix(path, "/echo/") {
-		expectedString, _ := strings.CutPrefix(path, "/echo/")
-		response = []byte(OK_RESPONSE_WITH_BODY + fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n", len(expectedString), expectedString))
-
-	} else if path == "/user-agent" {
-		response = []byte(OK_RESPONSE_WITH_BODY + fmt.Sprintf("Content-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s\r\n", len(userAgent), userAgent))
-	} else {
-		response = []byte(NOT_FOUND_RESPONSE)
-	}
-	connection.Write(response)
 }
